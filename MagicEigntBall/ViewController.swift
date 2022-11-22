d0f9abf6d65a70d26058930e822677d32c876217
Roman Zamolotov <rzamolotov@gmail.com>
2022-11-23 00:03:48 +0300

    Done program

--- MagicEigntBall/ViewController.swift
+++ MagicEigntBall/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var ballImage: UIImageView!
+    
+
+    @IBAction func answerButton(_ sender: UIButton) {
+        let ballArray = [UIImage(named: "ball1"),UIImage(named: "ball2"), UIImage(named: "ball3"), UIImage(named: "ball4"), UIImage(named: "ball5")]
+        
+        //solution 1
+        let randomNumber = Int.random(in: 0...4)
+        ballImage.image = ballArray[randomNumber]
+        
+        //solution 2
+//        ballImage.image = ballArray.randomElement()
+        
     }
-
-
+    
 }
 
